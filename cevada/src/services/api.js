f322c4163b32452a674ec0750df937765b9eb7ac
PedroHenriqueDeBarros <phbarrosr66@gmail.com>
2026-05-25 19:42:12 -0300

    Login

--- cevada/src/services/api.js
+++ cevada/src/services/api.js
@@ -1,12 +1,12 @@
 // cevada/src/services/api.js
 const BASE_URL = 'http://localhost:5000/api';
 
-export const loginUser = async (email, password) => {
+export const loginUser = async (email, idToken) => {
     try {
         const response = await fetch(`${BASE_URL}/login`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ email, password })
+            body: JSON.stringify({ email, idToken })
         });
         return await response.json();
     } catch (error) {
