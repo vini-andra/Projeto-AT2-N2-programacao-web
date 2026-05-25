f2dc6cbadcb37268dea50c0deff711cccd29d30e
PedroHenriqueDeBarros <phbarrosr66@gmail.com>
2026-05-25 19:54:12 -0300

    atualizando o github para o utilizar o .env

--- cevada/src/services/firebase.js
+++ cevada/src/services/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBLuT8t9BfvOeAbO5X7JTQomArVXL7Vz1M",
-  authDomain: "projetoextensaoweb.firebaseapp.com",
-  projectId: "projetoextensaoweb",
-  storageBucket: "projetoextensaoweb.firebasestorage.app",
-  messagingSenderId: "227778553447",
-  appId: "1:227778553447:web:a35041bbf1ee652744d6c2",
-  measurementId: "G-WSGD5KC5CG"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
